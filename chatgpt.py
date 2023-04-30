87e5635973d2a804270e810982c81e5a8baac7c3
mikefusc <mikefusc@me.com>
2023-04-29 22:05:37 -0400

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -32,7 +32,7 @@ def send_message_to_chatgpt(prompt, conversation_history):
 def upload_to_github(filename, content):
     g = Github(github_access_token)
     user = g.get_user()
-    repo = user.get_repo("YOUR_GITHUB_REPO_NAME")
+    repo = user.get_repo("Doc-generation")
 
     try:
         file = repo.get_contents(f"/docs/{filename}", ref="main")
@@ -40,38 +40,32 @@ def upload_to_github(filename, content):
     except:
         repo.create_file(f"/docs/{filename}", f"Create {filename}", content, branch="main")
 
-if __name__ == "__main__":
-    conversation_history = []
-    prompts = [
-        "Write me an executive summary in MD format.",
-        "Write me a high-level design doc in MD format.",
-        "Write me technical specs in MD format.",
-        "Write me an implementation guide in MD format.",
-        "Write me a test plan in MD format.",
-        "Write me user stories in MD format.",
-        "Write me test cases in MD format.",
-        "Write me a deployment guide.",
-        "Write me a project flow in mermaid.",
-        "Write me a 'things we still may need' list in MD format.",
-    ]
+initial_prompt = "Hi I just created a new github repo I would like your help with. The repo is a document generator that will call github api to create a new repo. Based on the user description of the repo, we will begin sending information to api asking for chatgpt to create specific files in .md format (Ex: test plan, high level design, tech specs, executive summary, readme)"
 
-    initial_prompt = "Hi I just created a new github repo I would like your help with. The repo is a document generator that will call github api to create a new repo. Based on the user description of the repo, we will begin sending information to api asking for chatgpt written in Python."
-    conversation_history.append({"role": "user", "content": initial_prompt})
-    response_message = send_message_to_chatgpt(initial_prompt, conversation_history)
-    if response_message:
-        conversation_history.append({"role": "assistant", "content": response_message})
-        print(f"ChatGPT: {response_message}")
+conversation_history = []
+response_message = send_message_to_chatgpt(initial_prompt, conversation_history)
+conversation_history.append({"role": "assistant", "content": response_message})
 
-    for index, prompt in enumerate(prompts):
-        prompt += " written in Python."
-        response_message = send_message_to_chatgpt(prompt, conversation_history)
-        if response_message:
-            conversation_history.append({"role": "user", "content": prompt})
-            conversation_history.append({"role": "assistant", "content": response_message})
-            print(f"ChatGPT: {response_message}")
+fixed_prompts = [
+    "write me an executive summary for this in Md format.",
+    "write me a high level design doc in Md format.",
+    "write me technical specs in .Md format.",
+    "write me an implementation guide in .Md format",
+    "write me a test plan in .Md format",
+    "write me user stories in Md format.",
+    "write me test cases in .Md format",
+    "write me a deployment guide",
+    "write me a project flow in mermaid",
+    "write me a things we still may need list in md format",
+]
 
-            if "MD format" in prompt:
-                filename = f"generated_file_{index}.md"
-                upload_to_github(filename, response_message, "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")
+for prompt in fixed_prompts:
+    time.sleep(20)  # Add rate limit (3 requests per minute)
+    
+    response_message = send_message_to_chatgpt(prompt, conversation_history)
+    conversation_history.append({"role": "user", "content": prompt})
+    conversation_history.append({"role": "assistant", "content": response_message})
 
-        time.sleep(20)  # Wait for 20 seconds between requests to maintain a rate limit of 
+    if "Md format" in prompt:
+        filename = prompt.split(" in ")[0].replace(" ", "_") + ".md"
+        upload_to_github(filename, response_message)
